da37c4dd1f65040a31ac2aac1d3a7cdf7c3442bb
Jing KE <jing.ke@myscript.com>
2021-12-13 14:46:41 +0100

    [lib][cppwinrt] initializations.

---
+++ src/Lib.CppWinRT/MyStruct.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include "MyStruct.h"
+#include "MyStruct.g.cpp"
+
+#include "Lib.Static.h"
+
+winrt::MyLib::implementation::MyStruct::MyStruct()
+    : mNative(std::make_shared<::MyLib::MyStruct>())
+{
+}
+
+double winrt::MyLib::implementation::MyStruct::Add(double x, double y)
+{
+    return mNative->add(x, y);
+}
+
+double winrt::MyLib::implementation::MyStruct::Substract(double x, double y)
+{
+    return mNative->substract(x, y);
+}
+
+double winrt::MyLib::implementation::MyStruct::Multiply(double x, double y)
+{
+    return mNative->multiply(x, y);
+}
+
+double winrt::MyLib::implementation::MyStruct::Divide(double x, double y)
+{
+    return mNative->divide(x, y);
+}
---
+++ src/Lib.CppWinRT/MyStruct.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "MyStruct.g.h"
+
+namespace MyLib
+{
+    struct MyStruct;
+}
+
+namespace winrt::MyLib::implementation
+{
+    struct MyStruct : MyStructT<MyStruct>
+    {
+        MyStruct();
+        double Add(double x, double y);
+        double Substract(double x, double y);
+        double Multiply(double x, double y);
+        double Divide(double x, double y);
+    private:
+        std::shared_ptr<::MyLib::MyStruct> mNative;
+    };
+}
+
+namespace winrt::MyLib::factory_implementation
+{
+    struct MyStruct : MyStructT<MyStruct, implementation::MyStruct>
+    {
+    };
+}
