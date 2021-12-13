54aa90b6cfa289eed04060f4668ad26e9666db73
Jing KE <jing.ke@myscript.com>
2021-12-13 14:12:38 +0100

    [lib][native] initializations.

---
+++ src/Lib.Static/Lib.Static.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "Lib.Static.h"
+
+double MyLib::MyStruct::add(double x, double y)
+{
+    return x + y;
+}
+
+double MyLib::MyStruct::substract(double x, double y)
+{
+    return x - y;
+}
+
+double MyLib::MyStruct::multiply(double x, double y)
+{
+    return x * y;
+}
+
+double MyLib::MyStruct::divide(double x, double y)
+{
+    return x / y;
+}
---
+++ src/Lib.Static/Lib.Static.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+namespace MyLib
+{
+    struct MyStruct
+    {
+        MyStruct() = default;
+        double add(double x, double y);
+        double substract(double x, double y);
+        double multiply(double x, double y);
+        double divide(double x, double y);
+    };
+}
