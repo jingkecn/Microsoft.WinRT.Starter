c6d4c9e925220293c32f898c5aec8e94bd686f84
Jing KE <jing.ke@myscript.com>
2021-12-14 14:42:32 +0100

    [app][uwp] make use of the C++/WinRT lib.

--- src/Main.UWP/MainPage.xaml.cs
+++ src/Main.UWP/MainPage.xaml.cs
@@ -1,5 +1,8 @@
+using System;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using MyLib;
+using Windows.UI.Popups;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -16,9 +19,12 @@ namespace Main.UWP
             InitializeComponent();
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private async void myButton_Click(object sender, RoutedEventArgs e)
         {
             myButton.Content = "Clicked";
+            var myStruct = new MyStruct();
+            var dialog = new MessageDialog($"1 + 1 = {myStruct.Add(1, 1)}", $"{myStruct}");
+            await dialog.ShowAsync();
         }
     }
 }
