df7def0007f5fc564db1bdd6b77880b0549aaeaf
Jing KE <jing.ke@myscript.com>
2021-12-29 14:31:23 +0100

    [app][maui] initializations.

---
+++ src/Main.MAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,12 @@
+﻿using Foundation;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+
+namespace Main.MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
