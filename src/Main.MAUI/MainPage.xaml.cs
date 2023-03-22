6756bca1a99e107d64d4f3bd7ef25502d1d79c33
Jing KE <jing.ke@myscript.com>
2023-03-22 16:26:09 +0100

    [app][maui] implementations.

--- src/Main.MAUI/MainPage.xaml.cs
+++ src/Main.MAUI/MainPage.xaml.cs
@@ -1,3 +1,5 @@
+using Lib.CppWinRT;
+
 namespace Main.MAUI
 {
     public partial class MainPage
@@ -9,11 +11,13 @@ namespace Main.MAUI
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void OnCounterClicked(object sender, EventArgs e)
         {
             _count++;
             CounterBtn.Text = _count == 1 ? $"Clicked {_count} time" : $"Clicked {_count} times";
             SemanticScreenReader.Announce(CounterBtn.Text);
+            var myStruct = new MyStruct();
+            await DisplayAlert($"{myStruct}", $"1 + 1 = {myStruct.Add(1, 1)}", "OK");
         }
     }
 }
